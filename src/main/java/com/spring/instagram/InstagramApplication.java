8982e33135c242cfb3c3038845b7f6de78ac89ab
Kim HyunWoo <stories2@naver.com>
2022-08-14 00:00:35 +0900

    add swagger support

--- src/main/java/com/spring/instagram/InstagramApplication.java
+++ src/main/java/com/spring/instagram/InstagramApplication.java
@@ -2,9 +2,17 @@ package com.spring.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class InstagramApplication {
+
+	@GetMapping("/")
+	String hellworld() {
+		return "OK.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstagramApplication.class, args);
