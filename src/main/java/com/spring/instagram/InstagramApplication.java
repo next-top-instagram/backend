6be4dd742c27490928aa2db1e58c1ec220967f35
Kim HyunWoo <stories2@naver.com>
2022-08-14 05:51:12 +0900

    asdf

--- src/main/java/com/spring/instagram/InstagramApplication.java
+++ src/main/java/com/spring/instagram/InstagramApplication.java
@@ -1,7 +1,11 @@
 package com.spring.instagram;
 
+import com.spring.instagram.student.Student;
+import com.spring.instagram.student.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +22,16 @@ public class InstagramApplication {
 		SpringApplication.run(InstagramApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+			Student maria = new Student(
+					"Maria",
+					"Jones",
+					"maria.jones@example.com",
+					21
+			);
+			studentRepository.save(maria);
+		};
+	}
 }
--- src/main/java/com/spring/instagram/student/Student.java
+++ src/main/java/com/spring/instagram/student/Student.java
@@ -55,6 +55,13 @@ public class Student {
     public Student() {
     }
 
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
     public Student(Long id, String firstName, String lastName, String email, Integer age) {
         this.id = id;
         this.firstName = firstName;
