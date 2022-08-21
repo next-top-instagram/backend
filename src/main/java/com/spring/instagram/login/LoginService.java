b77bc3ab2825759f46eff22a280428fb46ebb110
Kim HyunWoo <stories2@naver.com>
2022-08-21 10:19:53 +0900

    Create LoginService.java

---
+++ src/main/java/com/spring/instagram/login/LoginService.java
@@ -0,0 +1,22 @@
+package com.spring.instagram.login;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class LoginService {
+    @PostMapping
+    public void Login() {
+
+    }
+
+    @GetMapping
+    public void AmiLogin() {
+        
+    }
+
+    @DeleteMapping
+    public void Logout() {
+
+    }
+}
