60f9ec29483391af0ead1c00a0f1829eb669bfc0
Kim HyunWoo <stories2@naver.com>
2022-08-21 11:28:13 +0900

    Update LoginService.java

--- src/main/java/com/spring/instagram/login/LoginService.java
+++ src/main/java/com/spring/instagram/login/LoginService.java
@@ -1,22 +1,43 @@
 package com.spring.instagram.login;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+
+@RestController
+@RequestMapping(path = "/api/login")
 public class LoginService {
-    @PostMapping
-    public void Login() {
 
+    @Resource
+    private UserInfo userInfo;
+
+    @PostMapping
+    public String Login(@RequestBody LoginModel loginModel) {
+        if (loginModel.getEmail().equals("hello@example.com") && loginModel.getPassword().equals("1234")) {
+            userInfo.setUserNm(loginModel.getEmail());
+            userInfo.setUserId(1L);
+            return "OK";
+        }
+        return "Fail";
     }
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     @GetMapping
-    public void AmiLogin() {
-        
+    public String AmiLogin() {
+        log.info("asdf: " + userInfo.getUserNm());
+        if (userInfo.getUserNm() != null) {
+            return userInfo.getUserNm();
+        }
+        return "Fail";
     }
 
     @DeleteMapping
-    public void Logout() {
-
+    public String Logout(HttpSession session) {
+        session.invalidate();
+        return "Bye";
     }
 }
