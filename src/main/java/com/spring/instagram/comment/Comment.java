3f69bc7f36cde7416c504f142c1dc2f76f69cd83
minskkoasdsadgfsdkl <86870992+minskkoasdsadgfsdkl@users.noreply.github.com>
2022-08-28 12:35:36 +0900

    1차 생성

---
+++ src/main/java/com/spring/instagram/comment/Comment.java
@@ -0,0 +1,54 @@
+package com.spring.instagram.comment;
+
+public class Comment {
+
+    private int id;
+    private String name;
+    private String commentStr;
+
+    public Comment(int id, String name,String commentStr){
+        this.id = id;
+        this.name = name;
+        this.commentStr = commentStr;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+
+   public String getName()
+   {
+       return name;
+   }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommentStr()
+    {
+        return commentStr;
+    }
+
+    public void setCommentStr(String commentStr)
+    {
+        this.commentStr = commentStr;
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        return "Comment{" + "id=" + id + ", name='" + name +
+                '\'' + ", commentStr='" + commentStr + '}';
+    }
+
+}
