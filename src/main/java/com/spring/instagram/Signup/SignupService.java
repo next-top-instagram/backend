65177144e1cd77e483f5ae7a65f37a4804836107
kimtaegeun1 <xosldi0501@naver.com>
2022-09-18 10:37:40 +0900

    signup setting

--- src/main/java/com/spring/instagram/Signup/SignupService.java
+++ src/main/java/com/spring/instagram/Signup/SignupService.java
@@ -2,6 +2,8 @@ package com.spring.instagram.Signup;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,13 @@ public class SignupService {
         this.signupRepository = signupRepository;
     }
 
-    public
+    @PostMapping
+    public String FindEmail (@RequestBody Signup signup) {
+        if (signup.getEmail().equals("example@gmail.com")) {
+            return "중복된 이메일입니다.";
+        } else {
+
+        }
+
+    }
 }
