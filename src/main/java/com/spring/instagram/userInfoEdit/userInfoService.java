a0eccec8344398348c1950f13b0a31d59ccc2799
Kim HyunWoo <stories2@naver.com>
2022-09-25 10:54:56 +0900

    Update userInfoService.java

--- src/main/java/com/spring/instagram/userInfoEdit/userInfoService.java
+++ src/main/java/com/spring/instagram/userInfoEdit/userInfoService.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 @RequestMapping(path = "/api/userinfoedit")
-public class userinfoservice {
+public class userInfoService {
     @Resource
     private userInfo userinfo;
 
-    public userinfoservice(@RequestBody userInfo userinfo) {
+    public userInfoService(@RequestBody userInfo userinfo) {
         this.userinfo = userinfo;
         
     }
