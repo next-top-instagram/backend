5d28ebe482b1108dd5c1763d782e357702c3ae56
Kim HyunWoo <stories2@naver.com>
2022-10-16 05:34:00 +0900

    adsfasf

--- src/main/java/com/spring/instagram/SecurityConfig.java
+++ src/main/java/com/spring/instagram/SecurityConfig.java
@@ -1,29 +1,29 @@
-package com.spring.instagram;
+//package com.spring.instagram;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.web.cors.CorsConfiguration;
+//
+//import java.util.List;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.List;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter
-{
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("*"));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8080"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(List.of("*"));
-
-        // You can customize the following part based on your project, it's only a sample
-        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
-                .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
-
-    }
-}
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter
+//{
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedHeaders(List.of("*"));
+//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8080"));
+//        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.setExposedHeaders(List.of("*"));
+//
+//        // You can customize the following part based on your project, it's only a sample
+//        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+//                .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
+//
+//    }
+//}
