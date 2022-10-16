8175dcf5ad789ee15a72ceb5498cfe50f61e548c
Kim HyunWoo <stories2@naver.com>
2022-10-16 12:30:46 +0900

    asdfasfa

--- src/main/java/com/spring/instagram/Account/AccountService.java
+++ src/main/java/com/spring/instagram/Account/AccountService.java
@@ -1,17 +1,40 @@
 package com.spring.instagram.Account;
 
+import com.spring.instagram.models.Account;
+import com.spring.instagram.models.AccountRepository;
 import com.spring.instagram.models.BasicResponseModel;
 import com.spring.instagram.models.SignUpModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/account")
 public class AccountService {
 
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
     @PostMapping
     public BasicResponseModel signUp(@RequestBody SignUpModel data) {
 //        data.getEmail();
 //        data.getPassword();
-        return new BasicResponseModel(false, "Asdf", null);
+
+        Long cnt = accountRepository.findAccountByEmail(data.getEmail());
+        if (cnt > 0) {
+            return new BasicResponseModel(false, "Already exist email", null);
+        }
+        try {
+            accountRepository.save(new Account(data.getEmail(), data.getPassword()));
+            return new BasicResponseModel(true, "Welcome!", null);
+        } catch (Exception e) {
+            return new BasicResponseModel(false, e.getMessage(), null);
+        }
     }
 }
--- src/main/java/com/spring/instagram/models/AccountRepository.java
+++ src/main/java/com/spring/instagram/models/AccountRepository.java
@@ -1,6 +1,12 @@
 package com.spring.instagram.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query(value = "select count(*) from Account a where a.email = ?1", nativeQuery = true)
+    Long findAccountByEmail(String email);
 }
